4a9672cb388eb305d66d798602f418ba669a7578
Your Name <you@example.com>
2024-07-31 15:57:12 +0300

    v22 finish task

--- task/task/src/component/ProfileCard.jsx
+++ task/task/src/component/ProfileCard.jsx
@@ -201,6 +201,66 @@
 // };
 
 // export default ProfileCard;
+// import React, { useState } from 'react';
+// import { Button } from 'react-bootstrap';
+// import prof from './../assets/Rectangle 2504.png';
+// import icon from './../assets/Frame 44305.png';
+// import iss from './../assets/Frame 44292.png';
+// import fol from './../assets/fol.png';
+// import iconss from './../assets/Vector.png'
+
+
+// const ProfileCard = ({ isFollowed, setIsFollowed }) => {
+
+//     // const [isFollowed, setIsFollowed] = useState(false);
+//     const [showImage, setShowImage] = useState(false);
+//     const [showFollowers, setShowFollowers] = useState(false);
+//     const [bgColor, setBgColor] = useState('');
+//     const [profile, setProfile] = useState(''); 
+//     const [colorrr, setcolorrr] = useState('');
+//     const handleFollowClick = () => {
+       
+//         setIsFollowed(!isFollowed );
+//         setShowImage(!showImage);
+//         setShowFollowers(!showFollowers);
+//         setBgColor(!isFollowed ?   ' rgba(0 0, 0,0)' : ''); 
+//         setProfile(!isFollowed ?  '30px' :'')
+//         setcolorrr(!isFollowed ?  'white' :'')
+        
+       
+//     };
+    
+//     return (
+        
+//           <div className="profile-card"style={{ backgroundColor: bgColor }}>
+//             <img src={prof} alt="Profile" style={{ marginLeft:'5%',padding: '30px',background:colorrr}} />
+//                 <div className='content'  > 
+//                     <h2>
+//                         {showImage ? (
+//                             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
+//                                 <img style={{ width: '200px',height:'45px', marginBottom: '45px', position: 'absolute', left: '-30%' }} src={fol} alt="Follow Image" />
+//                                 <img style={{ width: '300px', position: 'absolute', left: '-20%', marginBottom: '1px' }} src={iss} alt="New Image" />
+//                             </div>
+//                         ) : (
+//                             <>
+//                                 <img src={icon} alt="Icon" />
+//                                 Digital Marketing
+//                             </>
+//                         )}
+//                     </h2>
+//                     <p>{showFollowers ? <p className='p-content'>More than 5000 followers</p>: 'Head of Digital Marketing'}</p>
+//                     <button style={{position:'relative'}} onClick={handleFollowClick} className="followButton" data-status={isFollowed ? "save" : "follow"}>
+//                      {isFollowed ? <><img src={iconss} alt="Save Icon" style={{bottom:'-22%',left:'-1%',position:'absolute',height:'20px',width: '15px',}} />Save Contact</> : 'Follow'}
+                        
+//                     </button>
+//                 </div>
+//             </div>
+       
+//     );
+// };
+
+// export default ProfileCard;
+
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import prof from './../assets/Rectangle 2504.png';
@@ -237,9 +297,9 @@ const ProfileCard = ({ isFollowed, setIsFollowed }) => {
                 <div className='content'  > 
                     <h2>
                         {showImage ? (
-                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
-                                <img style={{ width: '200px',height:'45px', marginBottom: '45px', position: 'absolute', left: '-30%' }} src={fol} alt="Follow Image" />
-                                <img style={{ width: '300px', position: 'absolute', left: '-20%', marginBottom: '1px' }} src={iss} alt="New Image" />
+                            <div className='div1'>
+                                <img className='img1' src={fol} alt="Follow Image" />
+                                <img className='img2'  src={iss} alt="New Image" />
                             </div>
                         ) : (
                             <>
@@ -250,7 +310,7 @@ const ProfileCard = ({ isFollowed, setIsFollowed }) => {
                     </h2>
                     <p>{showFollowers ? <p className='p-content'>More than 5000 followers</p>: 'Head of Digital Marketing'}</p>
                     <button style={{position:'relative'}} onClick={handleFollowClick} className="followButton" data-status={isFollowed ? "save" : "follow"}>
-                     {isFollowed ? <><img src={iconss} alt="Save Icon" style={{bottom:'-22%',left:'-1%',position:'absolute',height:'20px',width: '15px',}} />Save Contact</> : 'Follow'}
+                     {isFollowed ? <><img src={iconss} alt="Save Icon" style={{bottom:'-10%',left:'-1%',position:'absolute',height:'20px',width: '15px',}} />Save Contact</> : 'Follow'}
                         
                     </button>
                 </div>
